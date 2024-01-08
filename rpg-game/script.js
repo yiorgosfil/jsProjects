17ff79a380d04f0cb824cf9f48e39149f3b68a16
YogiYiorgos <yiorgosdev@gmail.com>
2024-01-08 21:20:14 +0100

    JSP RPG1 Dec8

---
+++ rpg-game/script.js
@@ -0,0 +1,94 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let weapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
+
+const text = document.querySelector('#text');
+const xpText = document.querySelector('#xpText');
+const healthText = document.querySelector('#healthText');
+const goldText = document.querySelector('#goldText');
+const monsterStats = document.querySelector('#monsterStats');
+const monsterName = document.querySelector('#monsterName');
+const monsterHealthText = document.querySelector('#monsterHealth');
+
+const weapons [
+  {name: 'stick', power: 5},
+  {name: 'dagger', power: 30},
+  {name: 'claw hammer', power: 50},
+  {name: 'sword', power: 100},
+  ];
+
+const monsters = [
+  {
+    name: 'slime',
+    level: 2,
+    health: 15
+  },
+  {
+    name: 'fanged beast',
+    level: 8,
+    health: 60
+  },
+  {
+    name: 'dragon',
+    level: 20,
+    health: 300
+  }
+]
+
+const locations = [
+  {
+    name: 'town square',
+    'button text': ['Go to store', 'Go to cave', 'Fight dragon'],
+    'button functions': [goStore, goCave, fightDragon],
+    text: 'You are in the town square. You see a sign that says "Store".'
+  },
+  {
+    name: 'store',
+    'button text': ['Buy 10 health (10 gold)', 'Buy weapon (30 gold)', 'Go to town square'],
+    'button functions': [buyHealth, buyWeapon, goTown],
+    text: 'You enter the store.'
+  },
+  {
+    name: 'cave',
+    'button text': ['Fight slime', 'Fight fanged beast', 'Go to town square'],
+    'button functions': [fightSlime, fightBeast, goTown],
+    text: 'You enter the cave. You see some monsters.'
+  },
+  {
+    name: 'fight',
+    'button text': ['Attack', 'Dodge', 'Run'],
+    'button functions': [attack, dodge, goTown],
+    text: 'Yoy are fighting a monster.'
+  },
+  {
+    name: 'kill monster',
+    'button text': ['Go to town square', 'Go to town square', 'Go to town square'],
+    'button functions': [goTown, goTown, goTown],
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+  },
+  {
+    name: 'town square',
+    'button text': ['Go to store', 'Go to cave', 'Fight dragon'],
+    'button functions': [goStore, goCave, fightDragon],
+    text: 'You are in the town square. You see a sign that says "Store".'
+  },
+  {
+    name: 'lose',
+    'button text': ['REPLAY?', 'REPLAY?', 'REPLAY?'],
+    'button functions': [restart, restart, restart],
+    text: 'You die 💀'
+  }
+];
+
+// Initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave
+button2.onclick = fightDragon;
