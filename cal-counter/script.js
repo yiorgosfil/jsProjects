cdb7986287d468373416f3ddd144eac2bc8a5200
yogiyiorgos <yiorgosdev@gmail.com>
2024-02-20 21:54:34 +0100

    JSP CC3 20Feb24

--- cal-counter/script.js
+++ cal-counter/script.js
@@ -15,3 +15,14 @@ function inInvalidInput(str) {
   const regex = /\d+e\d+/i
   return str.match(regex)
 }
+
+function addEntry() {
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`)
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length
+  const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Number</label>
+    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />
+  `
+}
