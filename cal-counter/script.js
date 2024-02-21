9d94c824edcef5e8185a6b5f178c516260250026
yogiyiorgos <yiorgosdev@gmail.com>
2024-02-21 22:27:13 +0100

    JSP CC4 21Feb24

--- cal-counter/script.js
+++ cal-counter/script.js
@@ -24,5 +24,15 @@ function addEntry() {
     <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
     <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
     input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />
-  `
+    `
+  targetInputContainer.insertAdjacentHTML('beforeend', HTMLString)
+}
+
+function getCaloriesFromInputs(list) {
+  let calories = 0
+
+  for (const item of list) {
+    const currVal = cleanInputString(item.value)
+    const invalidInputMatch = isInvalidInput(currVal)
+  }
 }
