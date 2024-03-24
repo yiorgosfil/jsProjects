33b4c9471957ed387274af1ab22d3da10e7b7d2e
yogiyiorgos <yiorgosdev@gmail.com>
2024-03-24 19:43:34 +0100

    JSP NS3 24Mar24

--- num-sort/script.js
+++ num-sort/script.js
@@ -39,5 +39,36 @@ function bubbleSort(array) {
 
 // Selection sort algorithm
 function selectionSort(array) {  
-  for (let i = 0; i < array.length; i++) 
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i
+  
+    // Find the index of the minimum element in the remaining unsorted portion
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j
+      }
+    }
+    // Swap the minimum element with the first element in the unsorted portion
+    const temp = array[i]
+    array[i] = array[minIndex]
+    array[minIndex] = temp
+  }
+  return array
 }
+
+// Insertion sort algorithm
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    const currValue = array[i]
+    let j = i - 1
+
+    while (j >= 0 && array[j] > currValue) {
+      array[j + 1] = array[j]
+      j--      
+    }
+    array[j + 1] = currValue
+  }
+  return array
+}
+
+sortButton.addEventListener('click', sortInputArray)
