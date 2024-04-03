1e272842e3b7de901bd93574980f829a3d01a122
yogiyiorgos <yiorgosdev@gmail.com>
2024-04-03 13:37:52 +0200

    JSP SC1 3Apr24

--- shop-cart/script.js
+++ shop-cart/script.js
@@ -85,6 +85,21 @@ const products = [
   },
 ]
 
+products.forEach(
+  ({ name, id, price, category }) => {
+    dessertCards.innerHTML += `
+      <div class="dessert-card">
+        <h2>${name}</h2>
+        <p class="dessert-price">$${price}</p>
+        <p class="product-category">Category: ${category}</p>
+        <button id="${id}" class=" btn add-to-cart-btn">
+          Add to cart
+        </button>
+      </div>
+    `
+  }
+)
+
 class ShoppingCart {
   constructor() {
     this.items = []
@@ -117,4 +132,63 @@ class ShoppingCart {
       </div>
     `
   }
+
+  getCounts() {
+    return this.items.length
+  }
+
+  clearCart() {
+    if (!this.items.length) {
+      alert('Your shopping cart is already empty')
+      return
+    }
+
+    const isCartCleared = confirm('Are you sure you want to clear all items from your shopping cart?')
+
+    if (isCartCleared) {
+      this.items = []
+      this.total = 0
+      productsContainer.innerHTML = ''
+      totalNumberOfItems.textContent = 0
+      cartSubTotal.textContent = 0
+      cartTaxes.textContent = 0
+      cartTotal.textContent = 0
+    }
+  }
+
+  calculateTaxes(amount) {
+    return parseFloat(((this.taxRate / 100) * amount).toFixed(2))
+  }
+
+  calculateTotal() {
+    const subTotal = this.items.reduce((total, item) => total + item.price, 0)
+    const tax = this.calculateTaxes(subTotal)
+    this.total = subTotal + tax
+    cartSubTotal.textContent = `$${subTotal.toFixed(2)}`
+    cartTaxes.textContent = `$${tax.toFixed(2)}`
+    cartTotal.textContent = `$${this.total.toFixed(2)}`
+    return this.total
+  }
 }
+
+const cart = new ShoppingCart()
+const addToCartBtns = document.getElementsByClassName('add-to-cart-btn')
+
+// Event Listeners //
+[addToCartBtns].forEach(
+  (btn) => {
+    btn.addEventListener('click', (event) => {
+      cart.addItem(Number(event.target.id), products)
+      totalNumberOfItems.textContent = cart.getCounts()
+      cart.calculateTotal()
+    })
+  }
+)
+
+cartBtn.addEventListener('click', () => {
+  isCartShowing = !isCartShowing
+  showHideCartSpan.textContent = isCartShowing ? 'Hide' : 'Show'
+  cartContainer.style.display = isCartShowing ? 'block' : 'none'
+})
+
+clearCartBtn.addEventListener('click', cart.clearCart.bind(cart))
