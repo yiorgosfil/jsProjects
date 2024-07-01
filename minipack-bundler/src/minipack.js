e90249981bc8c301faa23d508313608f7f09a0a9
Yiorgos <yiorgosdev@gmail.com>
2024-07-01 19:29:51 +0200

    MiniPack TODO: fix dependencies

--- minipack-bundler/src/minipack.js
+++ minipack-bundler/src/minipack.js
@@ -1,22 +1,97 @@
 const fs = require('fs');
 const path = require('path');
 const babylon = require('babylon');
-const traverse = require('babel-traverse').default;
-const { transformFromAst } = require('babel-core');
+const traverse = require('@babel/traverse');
+const { transformFromAst } = require('@babel/core');
 
 let ID = 0;
 
-// Accepts a path to a file, read its contents and extract its dependencies
+// Accepts a path to a file, reads its contents and extracts its dependencies
 function createAsset(filename) {
   const content = fs.readFileSync(filename, 'utf-8');
 
   const ast = babylon.parse(content, { sourceType: 'module' });
 
-  const dependencies = [];
+  const dependencies = []; // Holds the relative paths of the modules this module depends on
 
   traverse(ast, {
     ImportDeclaration: ({node}) => {
       dependencies.push(node.source.value);
     },
-  })
+  });
+
+  const id = ID++;
+
+  // Transpile the code with `babel`
+  const {code} = transformFromAst(ast, null, { presets: ['env'] });
+
+  return {
+    id,
+    filename,
+    dependencies,
+    code,
+  };
 }
+
+function createGraph(entry) {
+  const mainAsset = createAsset(entry);
+
+  const queue = [mainAsset];
+
+  for (const asset of queue) {
+    asset.mapping = {};
+
+    const dirname = path.dirname(asset.filename);
+
+    asset.dependencies.forEach(relativePath => {
+      const absolutePath = path.join(dirname, relativePath);
+      const child = createAsset(absolutePath);
+
+      asset.mapping[relativePath] = child.id;
+
+      queue.push(child);
+    })
+  }
+
+  return queue;
+}
+
+function bundle(graph) {
+  let modules = '';
+
+  graph.forEach(mod => {
+    modules += `#{mod.id}: [
+      function (require, module, exports) {
+        ${mod.code}
+      },
+      ${JSON.stringify(mod.mapping)},
+    ]`;
+  });
+
+  const result = `
+    (function(modules) {
+      function require(id) {
+        const [fn, mapping] = modules[id];
+
+        function localRequire(name) {
+          return require(mapping[name]);
+        }
+
+        const module = { exports: {} };
+
+        fn(localRequire, module, module.exports);
+
+        return module.exports;
+      }
+
+      return(0);
+    })({${modules}})
+  `;
+
+  return result;
+}
+
+const graph = createGraph('./example/entry.js');
+const result = bundle(graph);
+
+console.log(result);
